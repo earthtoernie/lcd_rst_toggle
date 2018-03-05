519caa1a09a7477013777886da3116204ac848ed
Ernie <ernie@earthtoernie.com>
2018-03-04 23:53:10 -0500

    now works perfectly to reset ST7565 lcd

--- pin_toggle/main.c
+++ pin_toggle/main.c
@@ -28,7 +28,11 @@ void init_avr(){
 	DDRB |= (1<<DDB0); // other pin as output
 	DDRB &= ~(1<<DDB2); // input to switch
 	PORTB |= (1<<PB2); // enable pull-up
-	PORTB |=(1<<PB0); // turn on the pin we use to strobe (its active low, so rest in high)
+	PORTB |=(1<<PB0); // turn on the pin we use to strobe (its active low, so rest is high)
+	
+	//Test pin from hw de-bounced button
+	DDRB &= ~(1<<DDB3);
+	
 	return;
 }
 
@@ -56,7 +60,7 @@ void poll_btn(){
 	}
 }
 
-// NOT de-bounced for demo purposes onle
+// NOT de-bounced for demo purposes only
 void bad_bounce(){
 	// trigger when we go from high to low (off to on)
 //	uint8_t last_button_value = (PINB & (1<<PINB2)) ? 1 : 0; //read the button
@@ -82,13 +86,13 @@ void good_bounce(){
 		if ((button_value == 1) & (in_wait_mode == 1)){
 			in_wait_mode = 0;
 			//toggle led
-			_delay_ms(50);
+			_delay_ms(50.0);
 			if((PINB & (1<<PINB2)) ? 0 : 1){
 				PORTB |= (1<<PB1); // turn on led
 				PORTB &= ~(1<<PB0); // drop 
-				_delay_us(2);
+				_delay_us(2.0);
 				PORTB |= (1<<PB0);
-				_delay_ms(1000);
+				_delay_ms(1000.0);
 				PORTB &= ~(1<<PB1); //turn off led
 				in_wait_mode = 1;
 			}
@@ -110,13 +114,13 @@ void btn_trigger(){
 		if ((button_value == 1) & (in_wait_mode == 1)){
 			in_wait_mode = 0;
 			//toggle led
-			_delay_ms(50);
+			_delay_ms(50.0);
 			if((PINB & (1<<PINB2)) ? 1 : 0){
 				PORTB |= (1<<PB1); // turn on led
 				PORTB &= ~(1<<PB0); // drop
-				_delay_us(2);
+				_delay_us(2.0);
 				PORTB |= (1<<PB0);
-				_delay_ms(1000);
+				_delay_ms(1000.0);
 				PORTB &= ~(1<<PB1); //turn off led
 				in_wait_mode = 1;
 			}
@@ -140,25 +144,67 @@ wait_for_btn(){
 			if ((button_value == 1) & (in_wait_mode == 1)){
 				in_wait_mode = 0;
 				//toggle led
-				_delay_ms(50);
+				_delay_ms(50.0);
 				if((PINB & (1<<PINB2)) ? 1 : 0){
 					PORTB |= (1<<PB1); // turn on led
-					PORTB &= ~(1<<PB0); // drop
-					_delay_us(2);
-					PORTB |= (1<<PB0);
-					_delay_ms(1000);
+
+					_delay_ms(1000.0);
 					PORTB &= ~(1<<PB1); //turn off led
 					in_wait_mode = 1;
+					return;
+
 				}
 				
 				} else {
 				in_wait_mode = 1;
-				_delay_ms(100);
+				_delay_ms(100.0);
 				PORTB ^= (1<<PB1);
 				in_wait_mode = 1;
 			}
 		}
+}
+
+// assumes the clock is active low, look at PB4
+void wait_for_clk_trigger(){
 	
+	while(1){
+		//wait for trigger from pin
+		// the clk should be high and we trigger when it goes low
+		uint8_t hitcount = 0;
+		uint8_t sclk_val = 0;
+		
+		//PB4 is where we r looking for sclk
+		break;
+	}
+	PORTB |= (1<<PB1); // turn on led
+	
+	PORTB &= ~(1<<PB0); // drop
+	_delay_us(2.0);
+	PORTB |= (1<<PB0);
+	_delay_ms(3000.0);
+	return;
+}
+
+// assumes the clock is active high, look at PB3, we have a hardware de-bounced switcher there (digilent Pmod BTN 4)
+void wait_for_clk_trigger_test(){
+	
+	PORTB |= (1<<PB1); // turn on led (to show user we are in this mode)
+	while(1){
+		//wait for trigger from pin
+		// the clk should be high and we trigger when it goes low
+		uint8_t hitcount = 0;
+		uint8_t pmod_btn_val = PINB & (1<<PINB3);
+		if(pmod_btn_val == 0){
+			PORTB &= ~(1<<PB1); //turn off led as indicator
+			_delay_us(14.0);
+			PORTB &= ~(1<<PB0); // drop
+			_delay_us(1.0);
+			PORTB |= (1<<PB0);
+			_delay_ms(1000.0); //bug here is the user re runs within a second
+			return;
+		}
+	}
+	return;
 }
 
 void trigger_low(){
@@ -169,7 +215,7 @@ void trigger_low(){
 		uint8_t button_value = (PINB & (1<<PINB2)) ? 0 : 1; //read the button
 		if (button_value == 1){
 			//toggle led
-			_delay_ms(50);
+			_delay_ms(50.0);
 			if((PINB & (1<<PINB2)) ? 1 : 0){
 				PORTB ^= (1<<PB1);
 				
@@ -182,6 +228,9 @@ void trigger_low(){
 int main(void)
 {
 	init_avr();
-	wait_for_btn();
+	while(1){
+		wait_for_btn();
+		wait_for_clk_trigger_test();
+	}
 }
 
